f1ccda56dd868f7c43286972e712f0acbad7a6e7
Daksh sharma <56217969+arkham7638@users.noreply.github.com>
2021-04-11 02:30:55 +0530

    Create wave array.java

---
+++ arrays/wave array.java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0)
+		{
+		    int n = Integer.parseInt(br.readLine().trim());// taking size of array
+		    int arr[] = new int[n]; // declaring array of size n
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr[i]=Integer.parseInt(inputLine[i]); // input elements of array
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    
+		    obj.convertToWave(arr, n);
+		    
+		    StringBuffer sb = new StringBuffer(); 
+            for (int i = 0; i < n; i++) 
+                sb.append(arr[i] + " "); 
+                
+		    System.out.println(sb); // print array
+		}
+	}
+}
+
+
+ // } Driver Code Ends
+class Solution{
+
+    
+    // arr: input array
+    // n: size of the array
+    //Function to sort the array into a wave-like array.
+    static void reverse(int arr[],int start,int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void convertToWave(int arr[], int n){
+        
+        // Your code here
+        for(int i = 0;i<n-1;i+=2){
+            reverse(arr,i,i+1);
+        }
+        
+    }
+    
+}
+
+
+// { Driver Code Starts.
+  // } Driver Code Ends
